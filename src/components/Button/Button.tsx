577b1b5285010fa5b0807ea76286346a5cf579cb
PreetiKharb <preets.khb@gmail.com>
2022-05-04 13:50:39 +0100

    fix: added new click handle

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,11 +3,12 @@ import './Button.css'
 
 interface ButtonProps {
     label: string, 
-    class: string
+    class: string,
+    onClick: () => void;
 }
  
 const Button = (props: ButtonProps) => {
-    return <button className={props.class}>{props.label}</button>
+    return <button className={props.class} onClick={props.onClick}>{props.label}</button>
 }
  
 export default Button;
